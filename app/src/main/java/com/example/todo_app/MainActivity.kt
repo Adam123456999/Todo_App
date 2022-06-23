a7834f30192de2fac9101d0bc1372ef7f4403615
Adam123456999 <adammambetov.342@gmail.com>
2022-06-23 21:24:18 +0300

    initial commit

---
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.todo_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todo_app.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    setSupportActionBar(binding.toolbar)
+
+    supportFragmentManager
+      .beginTransaction()
+      .add(R.id.nav_host_fragment_content_main, FirstFragment())
+      .commit()
+  }
+
+}
